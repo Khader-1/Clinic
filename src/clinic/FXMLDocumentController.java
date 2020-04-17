3281329a25c435dcf0cee21396cb6c7cab937d51
Khader-1 <khader2020kh@gmail.com>
2020-04-17 18:13:35 +0300

    added Transitions but really needs format '_'

--- src/clinic/FXMLDocumentController.java
+++ src/clinic/FXMLDocumentController.java
@@ -1,35 +1,89 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package clinic;
 
+import com.jfoenix.controls.JFXDrawer;
+import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
+import com.jfoenix.transitions.hamburger.HamburgerNextArrowBasicTransition;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.animation.Animation;
+import javafx.animation.ScaleTransition;
+import javafx.animation.Transition;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
-/**
- *
- * @author PC
- */
-public class FXMLDocumentController implements Initializable {
-    
-    @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+public class FXMLDocumentController implements Resizable {
+
+    final String content = "Lorem ipsum";
+
+    final Animation animation = new Transition() {
+        {
+            setCycleDuration(Duration.millis(500));
+        }
+
+        @Override
+        protected void interpolate(double frac) {
+            pane.setPrefWidth(50 + frac * 150);
+        }
+    };
+
+    final Animation inverseAnimation = new Transition() {
+        {
+            setCycleDuration(Duration.millis(500));
+        }
+
+        @Override
+        protected void interpolate(double frac) {
+            pane.setPrefWidth(200 - (frac * 150));
+        }
+    };
+
+    static void onChange(Number oldValue, Number newValue) {
+
     }
-    
+
+    @FXML
+    private JFXHamburger burger;
+
+    @FXML
+    private JFXDrawer drawer;
+
+    @FXML
+    private AnchorPane pane;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+
+        try {
+            drawer.setSidePane((AnchorPane) FXMLLoader.load(getClass().getResource("Pane.fxml")));
+        } catch (IOException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        HamburgerNextArrowBasicTransition back = new HamburgerNextArrowBasicTransition(burger);
+        back.setRate(back.getRate() * -1);
+        burger.setOnMouseClicked((event) -> {
+            back.setRate(back.getRate() * -1);
+            back.play();
+            if (back.getRate() > 0) {
+                drawer.open();
+                animation.play();
+            } else {
+                inverseAnimation.play();
+                drawer.close();
+            }
+        });
+    }
+
+    @Override
+    public void resize(Number old, Number newVal) {
+
+    }
+
 }
